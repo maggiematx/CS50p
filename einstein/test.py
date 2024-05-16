84d505d5e94d65f7332f9ba80867ef9f08a46f0f
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-16 23:43:28 +0000

    Fri, May 17, 2024, 7:43 AM +08:00

--- einstein/test.py
+++ einstein/test.py
@@ -11,14 +11,3 @@ def main():
 if __name__ == "__main__":
     main()
 
-def text():
-    converted_text=text.str.replace(":)", "smile").replace(":(","cry")
-    return converted_text
-
-def main():
-    use_input=input("How are you?")
-    converted=convert_text(uer_input)
-    print("good", converted)
-
-if __name__ == "__main__":
-    main()
