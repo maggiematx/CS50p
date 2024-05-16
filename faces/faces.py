c349faa1fefe5ec430a52198d7572770b114979d
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-16 03:40:43 +0000

    Thu, May 16, 2024, 11:40 AM +08:00

--- faces/faces.py
+++ faces/faces.py
@@ -1,11 +1,9 @@
 def convert()
-   
+
 
 
 def main():
- user_input = str(input("Please enter how you feel now "))
-    if user_input is in :)
-        then print(user_input)
-    print(lowercase_input)
+ user_input = input_str.replace(":)", "🙂")
+ 
 if __name__ == "__main__":
     main()
