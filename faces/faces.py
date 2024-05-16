bed90ab1e1091bdd7a18bafa10300d653c169a22
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-16 03:57:36 +0000

    Thu, May 16, 2024, 11:57 AM +08:00

--- faces/faces.py
+++ faces/faces.py
@@ -1,11 +1,11 @@
-def main():
-   user_input = str(input("How are you feeling now?"))
-    converted = convert(user_input)
-    print(converted)
-
-def convert()
+def convert(input_str)
     input=input_str.replace(":)", "🙂").replace(":(", "🙁")
     return input
+
+def main():
+   user_input = str(input("How are you feeling now?"))
+   converted = convert(user_input)
+   print(converted)
 
 if __name__ == "__main__":
     main()
