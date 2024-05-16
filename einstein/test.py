4a1e57695676fd1a861a242a781f9e19fa8597a5
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-16 23:15:55 +0000

    Fri, May 17, 2024, 7:15 AM +08:00

--- einstein/test.py
+++ einstein/test.py
@@ -1,11 +1,11 @@
-def E_cal():
+def E_cal(m,c):
     E = m * c ** 2
     return E
 
 def main():
     c = 3000000
     m = int(input("what's m: "))
-    energy = E_cal
+    energy = E_cal(m,c)
     print("E is:", energy)
 
 if __name__ == "__main__":
