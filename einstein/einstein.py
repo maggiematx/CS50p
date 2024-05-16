22625af5eacea4fdfe473efe2eaadd98dbc51de3
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-16 22:56:54 +0000

    Fri, May 17, 2024, 6:56 AM +08:00

--- einstein/einstein.py
+++ einstein/einstein.py
@@ -1,9 +1,9 @@
 # askign what's m
 m=int(input("What's m?"))
 # asking what's c
-c=int(input('What's c?'))
+c=int(input("What's c?"))
 # fomula to calculate E
-E=mc**2
+E=m*c**2
 #print E
 print(E)
 
