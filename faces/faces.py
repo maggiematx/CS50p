187a3f97679760101b643a8bfdade633e5872a60
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-16 03:33:21 +0000

    Thu, May 16, 2024, 11:33 AM +08:00

---
+++ faces/faces.py
@@ -0,0 +1,12 @@
+def main():
+    # Prompt the user for input
+    user_input = str(input("Please enter how you feel now "))
+
+    # Convert the input to lowercase
+    lowercase_input = user_input.lower()
+
+    # Output the lowercase input
+    print(lowercase_input)
+
+if __name__ == "__main__":
+    main()
