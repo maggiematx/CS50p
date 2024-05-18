e7c445eb62a8d1726df93b393bfce2c8d555af5a
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-18 07:59:11 +0000

    Sat, May 18, 2024, 3:59 PM +08:00

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -1,21 +1,14 @@
-def main():
-    math_interpreter()
 
+x, y, z = input("Expression: ").split()
+x = float(x)
+z = float(z)
+if y == "+":
+    print(f"{x+z:.1f}")
+elif y == "-":
+    print(f"{x-z:.1f}")
+if y == "*":
+    print(f"{x*z:.1f}")
+if y == "/":
+    print(f"{x/z:.1f}")
 
-def math_interpreter():
-    x, y, z = input("Expression: ").split()
-    x = float(x)
-    z = float(z)
-    if y == "+":
-        print(f"{x+z:.1f}")
-    elif y == "-":
-        print(f"{x-z:.1f}")
-    if y == "*":
-        print(f"{x*z:.1f}")
-    if y == "/":
-        print(f"{x/z:.1f}")
-
-
-if __name__ == "__main__":
-    main()
 
