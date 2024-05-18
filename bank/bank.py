b0d275ccba555edad4e7cadebc5187e09852c27b
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-18 02:14:53 +0000

    Sat, May 18, 2024, 10:14 AM +08:00

--- bank/bank.py
+++ bank/bank.py
@@ -1,6 +1,6 @@
-user_input=input("Greeting").strip().lower()
+user_input=input("Greeting: ").strip().lower()
 if user_input.startswith("hello"):
-    print("$0")"
+    print("$0")
 elif user_input.startswith("h") and user_input !="hello":
     print("20")
 else:
