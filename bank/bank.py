c2fbd8c6afe94f296a5c6877dac765f3a0a23699
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-18 02:16:17 +0000

    Sat, May 18, 2024, 10:16 AM +08:00

--- bank/bank.py
+++ bank/bank.py
@@ -2,6 +2,6 @@ user_input=input("Greeting: ").strip().lower()
 if user_input.startswith("hello"):
     print("$0")
 elif user_input.startswith("h") and user_input !="hello":
-    print("20")
+    print("$20")
 else:
     print("$100")
