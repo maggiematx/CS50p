26c03b59f6c087a96e4fcc1b7bec52b311fdd659
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-18 01:31:49 +0000

    Sat, May 18, 2024, 9:31 AM +08:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,3 +1,5 @@
 def main():
-    user_input=input("What is the Great Qquestion of Life, the universe, and everything?")
+    user_input=input("What is the Great Question of Life, the universe, and everything?")
+    if user_input is in "42", "forty-two", "forty two":
+        return 
     print(${tip:.2f}"))
