f680ab2c2b51f8f00b54d2629c1743f54a6b54fb
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-19 07:51:56 +0000

    Sun, May 19, 2024, 3:51 PM +08:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,10 +1,10 @@
 def main():
     time=input("what time is it now?")
-    if 7.0<=converted<=8.0:
+    if 7.0<=total_hours<=8.0:
         print("breakfast time")
-    elif 12.0<=converted <=13.0:
+    elif 12.0<=total_hours <=13.0:
         print("lunch time")
-    elif 18.0 <= converted <= 19.0:
+    elif 18.0 <= total_hours <= 19.0:
         print("dinner time")
     else:
         print("")
