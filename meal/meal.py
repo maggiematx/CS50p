81474a9bb3c983de3e936adfa9ff96a1e1e04a9d
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-19 02:17:52 +0000

    Sun, May 19, 2024, 10:17 AM +08:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,10 +1,19 @@
 def main():
-    ...
+    user_input=input()
+    if 7<=converted<=8:
+        print("breakfast time")
+    elif 12<=converted <=13:
+        print("lunch time")
+    elif 18 <= converted <= 19:
+        print("dinner time")
+    else:
+        print("")
 
 
 def convert(time):
     hours, minutes = time.split(":")
-    
+    return converted
+
 
 
 if __name__ == "__main__":
