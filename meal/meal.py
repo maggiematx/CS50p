98a15dbb741ad66d1ddaa8db515be95977fddea9
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-19 02:18:54 +0000

    Sun, May 19, 2024, 10:18 AM +08:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,5 +1,5 @@
 def main():
-    user_input=input()
+    time=input("what time is it now?")
     if 7<=converted<=8:
         print("breakfast time")
     elif 12<=converted <=13:
@@ -14,6 +14,8 @@ def convert(time):
     hours, minutes = time.split(":")
     return converted
 
+if __name__ == "__main__":
+    main()
 
 
 if __name__ == "__main__":
