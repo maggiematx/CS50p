0edc7202b34f26e176aa06be46d5584865a15b51
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-20 08:05:34 +0000

    Mon, May 20, 2024, 4:05 PM +08:00

--- test.py
+++ test.py
@@ -1,20 +1,30 @@
 def main():
-    print_square(3)
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
 
 
-def print_square(size):
+def is_valid(s):
+    valid = False
+    if len(s) > 1 and len(s) < 7:
+        start = s[0:2]
+        if s.isalpha():
+            valid = True
+        elif s.isalnum() and start.isalpha():
+            if not s[-1].isalpha():
+                for character in s[2:]:
+                    if character.isalpha():
+                        continue
+                    elif character.isdigit() and character != "0":
+                        valid = True
+                    break
+    return valid
 
-    # For each row in square
-    for i in range(size):
 
-        # For each brick in row
-        for j in range(size):
-
-            #  Print brick
-            print("#", end="")
-
-        # Print blank line
-        print()
+if __name__ == "__main__":
+    main()
 
 
 main()
