2a6726fc6a57d7517b001da17c8179eb953c97b5
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-20 08:40:46 +0000

    Mon, May 20, 2024, 4:40 PM +08:00

--- nutrition/nutrition.py
+++ nutrition/nutrition.py
@@ -1,1 +1,12 @@
-fruits={}
+fruits={"Apple":"130",
+        "Acocado":"50",
+        "Banana":"110",
+        "Cantaloupe":"50",
+        "Grapefruit":"60",
+        "Grapes":"90",
+        "Honeydew":"50",
+
+
+
+
+        }
