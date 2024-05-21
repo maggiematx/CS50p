e8f8b5a7322f40ec13869cc0651c434599e27c3c
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-21 01:06:16 +0000

    Tue, May 21, 2024, 9:06 AM +08:00

--- nutrition/nutrition.py
+++ nutrition/nutrition.py
@@ -1,10 +1,10 @@
 fruits=[{"Item":"Apple","Calories":"130"},
-        {"Item":"Acocado":"50","Calories":"50"},
-        {"Item":"Banana":"110","Calories":"110"},
-        {"Item":"Cantaloupe":"50","Calories":"50"},
-        {"Item":"Grapefruit":"60","Calories":"60"},
-        {"Item":"Grapes":"90","Calories":"90"},
-        {"Item":"Honeydew":"50","Calories":"50"},
+        {"Item":"Acocado","Calories":"50"},
+        {"Item":"Banana","Calories":"110"},
+        {"Item":"Cantaloupe","Calories":"50"},
+        {"Item":"Grapefruit","Calories":"60"},
+        {"Item":"Grapes","Calories":"90"},
+        {"Item":"Honeydew","Calories":"50"},
 
 
 ]
