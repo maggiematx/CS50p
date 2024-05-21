f2b5d9d03d221ed045d585282f07c71489c185bb
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-21 01:16:02 +0000

    Tue, May 21, 2024, 9:16 AM +08:00

--- nutrition/nutrition.py
+++ nutrition/nutrition.py
@@ -5,9 +5,22 @@ fruits=[{"Item":"Apple","Calories":"130"},
         {"Item":"Grapefruit","Calories":"60"},
         {"Item":"Grapes","Calories":"90"},
         {"Item":"Honeydew","Calories":"50"},
-
-
+        {"Item":"Kiwifruit","Calories":"90"},
+        {"Item":"Lemon","Calories":"15"},
+        {"Item":"Lime","Calories":"20"},
+        {"Item":"Nectarine","Calories":"60"},
+        {"Item":"Orange","Calories":"80"},
+        {"Item":"Peach","Calories":"60"},
+        {"Item":"Pear","Calories":"100"},
+        {"Item":"Pineapple","Calories":"50"},
+        {"Item":"Plums","Calories":"70"},
+        {"Item":"strawberries","Calories":"50"},
+        {"Item":"Sweet Cherries","Calories":"100"},
+        {"Item":"Tangerine","Calories":"50"},
+        {"Item":"Watermelon","Calories":"80"},
 ]
+
 ueser_input=input("Item: ")
-for user_input in fruits:
-    print(fruits["Calories"])
+for fruit in fruits:
+    if user_input=fruit["Item"]
+    print(f"Calories: , {fruit["Calories"]}")
