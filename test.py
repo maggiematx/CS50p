d1229f82e9d8282f99027b83e76f80faa47c403f
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-21 03:28:33 +0000

    Tue, May 21, 2024, 11:28 AM +08:00

--- test.py
+++ test.py
@@ -7,11 +7,14 @@ def print_square(size):
     # For each row in square
     for i in range(size):
 
+        # For each brick in row
+        for j in range(size):
 
             #  Print brick
             print("#", end="")
+        print()
 
-   
+
 
 
 main()
