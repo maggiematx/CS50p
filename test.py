df50d76d1b90996e3d28e320bbea83193a5e09d7
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-21 03:18:49 +0000

    Tue, May 21, 2024, 11:18 AM +08:00

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
-students = ["Hermoine", "Harry", "Ron"]
-
-for i in range(students):
-    print(i + 1, students[i])
+students = {
+    "Hermoine": "Gryffindor",
+    "Harry": "Gryffindor",
+    "Ron": "Gryffindor",
+    "Draco": "Slytherin",
+}
+for student in students:
+    print(student, students[student])
