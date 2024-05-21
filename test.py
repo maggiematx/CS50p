f0b81c36c8544ecdf6267f68e41e3d03cfba85cf
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-21 08:33:45 +0000

    Tue, May 21, 2024, 4:33 PM +08:00

--- test.py
+++ test.py
@@ -4,11 +4,11 @@ def main():
 
 def amount_due():
     while amount_due>0:
-        print(f"Amount Due: ,{amount_due})
+        print(f"Amount Due: ,{amount_due}")
 
         coin=input("Insert Coin: ")
 
-        if coin in [25,10,5]
+        if coin in [25,10,5]:
             amount_due -= coin
         if amount_due <= 0:
              print(f"Change Owed: {amount_due * -1}")
