e7a333884977d1dbd986edc0a32dea1b79b1c50d
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-21 03:51:04 +0000

    Tue, May 21, 2024, 11:51 AM +08:00

--- test.py
+++ test.py
@@ -2,9 +2,9 @@ def main():
     print_square(3)
 
 
-def print_square(size):
-    for i in range(size):
-        print_row(size)
+def print_square(n):
+    for i in range(n):
+        print_row(n)
 
 
 
