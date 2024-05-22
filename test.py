6840f24ac335e5d2df0d24b5ee5612d6a186df5c
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-22 03:22:50 +0000

    Wed, May 22, 2024, 11:22 AM +08:00

--- test.py
+++ test.py
@@ -1,14 +1,10 @@
-def main():
-    x = get_int("What's x? ")
-    print(f"x is {x}")
-
-
-def get_int(prompt):
-    while True:
-        try:
-            return int(input(prompt))
-        except ValueError:
-            pass
+while True:
+    try:
+        x=int(input("waht's x"))
+        print(f'x is {x}')
+    except ValueError:
+        print('x is not an integer)
+    
 
 
 main()
