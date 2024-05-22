a1ecc1f95ab96bbaa76069dedfa13c8776b74e90
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-22 08:11:47 +0000

    Wed, May 22, 2024, 4:11 PM +08:00

--- test.py
+++ test.py
@@ -1,22 +1,16 @@
-def main():
-    x = get_int()
-    print(f"x is {x}")
 
-
-def get_int():
-    while True:
-        try:
-            Fraction= int(input("Fraction: "))*100%
-        except (ValueError, ZeroDivisionError):
-            pass
-        else:
-            if Fraction >1:
-                print("f {Fraction}")
-            elif Fraction => .99 and Fraction =<1
-                print('F')
-            elif Fraction =<0.01:
-                print('E')
-
+while True:
+    try:
+        Fraction= int(input("Fraction: "))*100%
+    except (ValueError, ZeroDivisionError):
+        pass
+    else:
+        if Fraction >1:
+            print("f {Fraction}")
+        elif Fraction => .99 and Fraction =<1
+            print('F')
+        elif Fraction =<0.01:
+            print('E')
 
 main()
 
