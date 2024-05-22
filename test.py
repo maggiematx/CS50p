6f5861fc76262b475ee177b867eb359872640aa7
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-22 01:52:10 +0000

    Wed, May 22, 2024, 9:52 AM +08:00

--- test.py
+++ test.py
@@ -1,8 +1,13 @@
 
+def main():
+    User_input=str(input("Input: ")).lower()
+    final=shorted(user_input)
 
-
-User_input=str(input("Input: ")).lower()
-
-if c in [a, e, i,o, u]:
-    print
+def shorted(user_input):
+    v=[a, e, i,o, u]
+    nv=""
+    if c is not in v:
+        nv += c
+    elif c is in v:
+        print('nv')
 
