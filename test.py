df121551e8463e075568c6bafb5d9378194ebe16
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-22 02:28:15 +0000

    Wed, May 22, 2024, 10:28 AM +08:00

--- test.py
+++ test.py
@@ -1,15 +1,2 @@
-
-def main():
-    user_input=str(input("Input: ")).lower()
-    output=shorted(user_input)
-    print(f"Output: {output}")
-
-def shorted(user_input):
-    v=["a", "e", "i","o", "u"]
-    nv=""
-    for c in user_input:
-        if c not in v:
-            nv += c
-    return nv
-main()
-
+x=int(input("what's x?"))
+print(f"x is {x}")
