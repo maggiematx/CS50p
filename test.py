d2ccb3433ed3ade398327cc010486f29137dc21b
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-22 03:40:00 +0000

    Wed, May 22, 2024, 11:40 AM +08:00

--- test.py
+++ test.py
@@ -1,7 +1,5 @@
-while True:
-    try:
-        x=int(input("waht's x"))
-        print(f'x is {x}')
-        
-    except ValueError:
-        print('x is not an integer')
+try:
+    x=int(input("waht's x"))
+except ValueError:
+    print('x is not an integer')
+print(f'x is {x}')
