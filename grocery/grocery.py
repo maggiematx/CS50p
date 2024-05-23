8c2bfa41d1efd29d4cffffe1a909296d3633cf72
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-23 08:04:57 +0000

    Thu, May 23, 2024, 4:04 PM +08:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -1,8 +1,13 @@
-dict {APPLE, BANANA, ICE CREAM
+dict {"APPLE" : 1,
+      "BANANA":1,
+      "ICE CREAM": 1,
+      "LETTUE": 1,
       }
 
 while True:
     user_input=input("").upper()
 
     if user_input in dict:
+        count[dic] += 1
         
+
