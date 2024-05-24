8866c9076a68d2843f5bbe3e2dd80bab32ee3af5
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-24 01:33:24 +0000

    Fri, May 24, 2024, 9:33 AM +08:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -1,11 +1,26 @@
 def main():
 
 def ():
+user_month=[
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
     user_input=input("Date: ")
     if "/" in user_input:
         input_date=user_input.strtok("/")
     else:
-        input_date=input.strtok(" ")
+        if usert_input in user_month:
+            input_date=input.strtok(" ")
 
 
 
