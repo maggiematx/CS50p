89e3167db99f6479068944418155ac1abc88e6e9
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-24 01:30:24 +0000

    Fri, May 24, 2024, 9:30 AM +08:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -3,6 +3,9 @@ def main():
 def ():
     user_input=input("Date: ")
     if "/" in user_input:
-        input_date=
+        input_date=user_input.strtok("/")
+    else:
+        input_date=input.strtok(" ")
+
 
 
