3cccab157b3b05250be8878333216347f9c2f0cb
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-25 15:30:57 +0000

    Sat, May 25, 2024, 11:30 PM +08:00

--- test.py
+++ test.py
@@ -1,16 +1,6 @@
-def main():
-
-    # Output using our own function
-    name = input("What's your name? ")
-    hello(name)
-
-    # Output without passing the expected arguments
-    hello()
-
 
 # Create our own function
 def hello(to):
     print("hello,", to)
 
 
-main()
