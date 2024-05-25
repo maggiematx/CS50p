0aba1a13de4d65173f8e11ac7b9e4c9b377ed466
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-25 01:53:39 +0000

    Sat, May 25, 2024, 9:53 AM +08:00

--- test.py
+++ test.py
@@ -1,10 +1,26 @@
+months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    ]
+
 date=input("Date: ")
 if "/" in date:
     month, day, year = date.split("/")
 else:
     month, day, year = date.replace(",", "").split()
-    
+
 if month in months:
                 month = months.index(month) + 1
-            else:
-                continue
+print(f"{year}-{month:02}-{day:02}")
+
+
