2ca9155cf79fdeb05267b2619a6113e25fdad2bb
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-25 09:16:59 +0000

    Sat, May 25, 2024, 5:16 PM +08:00

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
-print("hello, \"friend\"")
+# Ask the user for their name
+name = input("What's your name? ")
+
+# Remove whitespace from the str
+name = name.strip()
+
+# Capitalize the first letter of each word
+name = name.title()
+
+# Print the output
+print(f"hello, {name}")
