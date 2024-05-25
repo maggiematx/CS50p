a17f824cf0e01c93836ac4375211eb12226762f0
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-25 01:51:29 +0000

    Sat, May 25, 2024, 9:51 AM +08:00

--- test.py
+++ test.py
@@ -3,4 +3,8 @@ if "/" in date:
     month, day, year = date.split("/")
 else:
     month, day, year = date.replace(",", "").split()
-print(month, day, year)
+    
+if month in months:
+                month = months.index(month) + 1
+            else:
+                continue
