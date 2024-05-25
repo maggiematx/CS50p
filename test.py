5bcbf5ad9bb3a0d5f82b4d8119dae5c94d2ea6c7
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-25 02:05:20 +0000

    Sat, May 25, 2024, 10:05 AM +08:00

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
-months = [
+def main():
+    format_date()
+
+
+def format_date():
+    months = [
         "January",
         "February",
         "March",
@@ -12,15 +17,30 @@ months = [
         "November",
         "December"
     ]
+    month = day = year = ""
 
-date=input("Date: ")
-if "/" in date:
-    month, day, year = date.split("/")
-else:
-    month, day, year = date.replace(",", "").split()
-
-if month in months:
+    while True:
+        date = input("Date: ")
+        if "/" in date:
+            month, day, year = date.split("/")
+        else:
+            month, day, year = date.replace(",", "").split()
+            if month in months:
                 month = months.index(month) + 1
-print(f"{year}-{month:02}-{day:02}")
+            else:
+                continue
+        try:
+            day = int(day)
+            year = int(year)
+            month = int(month)
+            if day > 31 or month > 12:
+                continue
+        except ValueError:
+            continue
+        break
+
+    print(f"{year}-{month:02}-{day:02}")
 
 
+if __name__ == "__main__":
+    main()
