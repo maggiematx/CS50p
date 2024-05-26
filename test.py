eefd38af5e151589bf0a5d9b37d60da9253cfe0c
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-26 15:09:39 +0000

    Sun, May 26, 2024, 11:09 PM +08:00

--- test.py
+++ test.py
@@ -1,16 +1,5 @@
-def main():
-    dollars = dollars_to_float(input("How much was the meal? "))
-    percent = percent_to_float(input("What percentage would you like to tip? "))
-    tip = dollars * percent
-    print(f"Leave ${tip:.2f}")
+x = int(input("What's x? "))
+y = int(input("What's y? "))
 
-
-def dollars_to_float(d):
-    return round(float(d.strip("$")), 1)
-
-
-def percent_to_float(p):
-    return float(p.strip("%"))/100
-
-
-main()
+if x < y:
+    print("x is less than y")
