124888315564f79657a8a56c55378161e03c9a63
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-27 03:24:58 +0000

    Mon, May 27, 2024, 11:24 AM +08:00

--- test.py
+++ test.py
@@ -1,23 +1,2 @@
-def main():
-    calculation()
-
-
-def calculation():
-      x, y,z= input("Expression: ").split()
-      x=int(x)
-      z=int(z)
-      if y=="+":
-            print(f"{x+z: .1f}")
-      elif y=="-":
-            print(f"{x-z: .1f}")
-      elif y=="*":
-            print(f"{x*z: .1f}")
-      elif y=="/" and z!=0:
-            print(f"{x/z:.1f}")
-      else:
-           print("Invalid input.")
-
-
-
-if __name__ == "__main__":
-    main()
+input_str = input("")
+x, y, z = input_str.split(" ")
