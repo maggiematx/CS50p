ad384bf076d6f39fa1e14431d52644f2a450de3a
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-27 01:57:56 +0000

    Mon, May 27, 2024, 9:57 AM +08:00

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-user_input=input("Greeting: ").lower().strip()
+user_input=input("File name: ").lower().strip()
 if user_input=="Hello":
       print(0)
 elif user_input.startswith ("h") and user_input != "hello":
