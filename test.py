3740e2a765a5d1a66cc845bcfe2a2022feec1991
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-27 01:31:23 +0000

    Mon, May 27, 2024, 9:31 AM +08:00

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 user_input=input("Greeting: ").lower().strip()
-      if user_input=="Hello"
-            print(0)
-      elif user_input startswith "h" and != "hello"
-            print("$20")
-      else:
-            pring("$100")
+if user_input=="Hello":
+      print(0)
+elif user_input.startswith ("h") and user_input != "hello":
+      print("$20")
+else:
+      print("$100")
 
