eab391ebd59af35d6ab5c7eb7e8dcbba336f3d22
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-27 03:51:53 +0000

    Mon, May 27, 2024, 11:51 AM +08:00

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 def main():
       time=input("What tie is it?")
+      total_time=convert(time)
       if 7<=total_time<=8:
             print("breakfast time")
       elif 12<=total_time<=13:
@@ -7,13 +8,14 @@ def main():
       elif 18<=total_time<=19:
             print("dinner time")
       else:
-
+            pass
 
 def convert(time):
       hours, minutes=time.split(":")
       hours=int(hours)
       minutes=int(minutes)
-      total_time=hours+minutes/60
+      total_time=hours+minutes/6
+      return total_time
 
 
 main()
