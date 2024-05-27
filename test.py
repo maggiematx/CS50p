a228e8f4d53ba11b8cc08fda83ec1643063e3246
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-27 02:57:29 +0000

    Mon, May 27, 2024, 10:57 AM +08:00

--- test.py
+++ test.py
@@ -3,14 +3,14 @@ def main():
 
 
 def calculation():
+      x, y,z= input("Expression: ").split(" "):
 
-    x, y,z= input("Expression: ").split(" ")
-    print(f"{z:.1f}")
-    if x and z are int, y is in ("=","-","*","/"):
-    if y=="/" and z!=0:
-      print
-    else:
-        print("application/octet-stream")
+      if x and z are int, y is in ("=","-","*","/"):
+            calculation= x y z
+      if y=="/" and z!=0:
+            print
+      else:
+            print("application/octet-stream")
 
 if __name__ == "__main__":
     main()
