87061085a79e784d61d3f08348bba25c3e77c4ba
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-27 02:43:32 +0000

    Mon, May 27, 2024, 10:43 AM +08:00

--- test.py
+++ test.py
@@ -1,18 +1,10 @@
 def main():
-    extensions()
+    calculation()
 
 
-def extensions():
-    user_input = input("File name: ").lower().strip().split(".")
-    extension = {
-        "gif": "image/gif",
-        "jpg": "image/jpeg",
-        "jpeg": "image/jpeg",
-        "png": "image/png",
-        "pdf": "application/pdf",
-        "txt": "text/plain",
-        "zip": "application/zip",
-    }
+def calculation():
+    user_input = int(input("Expression: ")).split(".")
+
     if user_input[1] in extension:
         print(extension[user_input[1]])
     else:
