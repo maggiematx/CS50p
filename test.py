5f370fbd15e81416f166a7f88374c692cc18b430
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-27 01:08:32 +0000

    Mon, May 27, 2024, 9:08 AM +08:00

--- test.py
+++ test.py
@@ -1,10 +1,4 @@
-name = input("What's your name? ")
-
-match name:
-    case "Harry" | "Hermione" | "Ron":
-          print("Gryffindor")
-    case "Draco":
-          print("Slytherin")
-    case _:
-          print("Who?")
+user_input=input("What is the Answer to the Great Question of Life, the Universe, and Everything?").lower()
+if "42" in user_input or "forty-two" in user_input or "forty two"  in user_input:
+    print("Yes")
 
