d607f9599b362e9c82a59ac356834d1a5fa15987
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-27 02:01:59 +0000

    Mon, May 27, 2024, 10:01 AM +08:00

--- test.py
+++ test.py
@@ -1,8 +1,12 @@
 user_input=input("File name: ").lower().strip()
-if user_input=="Hello":
-      print(0)
-elif user_input.startswith ("h") and user_input != "hello":
-      print("$20")
+if ".git" in user_input:
+      print("imge/gif")
+elif ".jpg" in user_input or ".jpeg" in user_input:
+      print("imge/jpeg")
+elif ".png" in user_input:
+      print("imge/png")
+elif if ".pdf" in user_input:
+      print("application/pdf")
 else:
-      print("$100")
+      print("application/octet-stream")
 
