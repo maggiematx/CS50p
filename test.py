814065ff38d8782011c1e9bc79c4b282ab25a2e1
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-27 10:38:34 +0000

    Mon, May 27, 2024, 6:38 PM +08:00

--- test.py
+++ test.py
@@ -1,23 +1,4 @@
-def main():
-      time=input("What time is it?")
-      total_time=convert(time)
-      if 7<=total_time<=8:
-            print("breakfast time")
-      elif 12<=total_time<=13:
-            print("lunch time")
-      elif 18<=total_time<=19:
-            print("dinner time")
-      else:
-            pass
+import random
 
-def convert(time):
-      hours, minutes=time.split(":")
-      hours=int(hours)
-      minutes=int(minutes)
-      total_time=hours+minutes/6
-      return total_time
-
-
-main()
-
-fljkffsjkljifjlasfjfasjlkjfffasffasffsas
+coin = random.choice(["heads", "tails"])
+print(coin)
