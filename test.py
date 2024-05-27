47bf295d058acc6700f3819c774e9dd02efde07b
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-27 02:15:50 +0000

    Mon, May 27, 2024, 10:15 AM +08:00

--- test.py
+++ test.py
@@ -1,2 +1,22 @@
-user_input = input("File name: ").lower().strip().split(".")
-print(user_input)
+def main():
+    extensions()
+
+
+def extensions():
+    user_input = input("File name: ").lower().strip().split(".")
+    extension = {
+        "gif": "image/gif",
+        "jpg": "image/jpeg",
+        "jpeg": "image/jpeg",
+        "png": "image/png",
+        "pdf": "application/pdf",
+        "txt": "text/plain",
+        "zip": "application/zip",
+    }
+    if user_input[text]] in extension:
+        print(extension[user_input[text]])
+    else:
+        print("application/octet-stream")
+
+if __name__ == "__main__":
+    main()
