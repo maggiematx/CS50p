f56e393826fa0b4ae35b0b28088f6e4cb1021ea7
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-27 03:39:04 +0000

    Mon, May 27, 2024, 11:39 AM +08:00

--- test.py
+++ test.py
@@ -1,3 +1,13 @@
-input_str = input("")
-x, y, z = input_str.split(" ")
-1 + 1
+def main():
+      time=input("What tie is it?")
+
+def convert(time):
+      if 7:00<=time<=8:00:
+            print("breakfast time")
+      elif 12:00<=time<=13:00:
+            print("lunch time")
+      elif 18:00<=time<=19:00:
+            print("dinner time")
+      else:
+            
+
