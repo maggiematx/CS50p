c54934df2ca5183872132fd7dd2b91142ecc58a9
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-27 02:04:09 +0000

    Mon, May 27, 2024, 10:04 AM +08:00

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ elif ".jpg" in user_input or ".jpeg" in user_input:
       print("imge/jpeg")
 elif ".png" in user_input:
       print("imge/png")
-elif if ".pdf" in user_input:
+elif ".pdf" in user_input:
       print("application/pdf")
 else:
       print("application/octet-stream")
