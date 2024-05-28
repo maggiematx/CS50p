d2ca7846f941adc6407f93f00676e54fa7b93ccc
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-28 02:43:37 +0000

    Tue, May 28, 2024, 10:43 AM +08:00

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
-import sys
+from sys import exit
 
 if len(sys.argv) < 2:
-    sys.exit("Too few arguments")
+    exit("Too few arguments")
 elif len(sys.argv) > 2:
-    sys.exit("Too many arguments")
+    exit("Too many arguments")
 
 print("hello, my name is", sys.argv[1])
