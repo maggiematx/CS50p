f33457b0a312e6ee99f224a800df012fa144da0f
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-28 03:39:11 +0000

    Tue, May 28, 2024, 11:39 AM +08:00

--- test.py
+++ test.py
@@ -1,7 +1,3 @@
-import sys
-
-if len(sys.argv) < 2:
-    sys.exit("Too few arguments")
-
-for arg in sys.argv[:-1]:
-    print("hello, my name is", arg)
+fruits = ["apple", "banana", "cherry"]
+fruitif fruit in fruits:
+    print(fruit)
