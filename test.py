c6ac8276a605fdf757a4fe1a049e3674bc832d44
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-28 08:14:40 +0000

    Tue, May 28, 2024, 4:14 PM +08:00

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import json
 import requests
 import sys
 
-if len(sys.argv) != 2:
+if len(sys.argv) != 3:
     sys.exit()
 
 response = requests.get("https://itunes.apple.com/search?entity=song&limit=10&term=" + sys.argv[1])
