88cf5bbbe03087b16ed7ea3bbdacf541223f9e77
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-29 04:14:23 +0000

    Wed, May 29, 2024, 12:14 PM +08:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -3,29 +3,26 @@ import random
 import sys
 
 
-
 def main():
     figlet = Figlet()
 
-
-    if len(sys.argv)==2 or len(sys.argv)>3:
+    if len(sys.argv) == 2 or len(sys.argv) > 3:
         sys.exit("Invalid usage")
 
+    if len(sys.argv) == 1:
+        font = random.choice(figlet.getFonts())
 
-
-    if len(sys.argv)==1:
-         font=random.choice(figlet.getFonts())
-
-    elif len(sys.argv)==3:
-        if sys.argv[1]== "-f" or sys.argv[1]=="--font":
-            font=sys.argv[2]
+    elif len(sys.argv) == 3:
+        if sys.argv[1] == "-f" or sys.argv[1] == "--font":
+            font = sys.argv[2]
             if font not in figlet.getFonts():
                 sys.exit("Invalid usage")
         else:
             sys.exit("Invalid usage")
 
-
     figlet.setFont(font=font)
     user_input = input("Input: ")
     print("Output: ", figlet.renderText(user_input))
+
+
 main()
