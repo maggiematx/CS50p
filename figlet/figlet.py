885c3cb8b301dff85a202ce2712884272242b8d7
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-29 03:19:12 +0000

    Wed, May 29, 2024, 11:19 AM +08:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,11 +1,13 @@
 from pyfiglet import Figlet
 import random
-import sys
 
 def main():
-    
-figlet = Figlet()
-if len(sys.argv)==0:
+    figlet()
+
+def figlet():
+    figlet = Figlet()
+    user_input=input("Input: ")
+    if len(sys.argv)==0:
     figlet
 
 figlet.getFonts()
