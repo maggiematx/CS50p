e0f35d76397f34622e77d1d2ead8212a9810c142
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-29 14:05:57 +0000

    Wed, May 29, 2024, 10:05 PM +08:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -9,6 +9,6 @@ def name():
             user_input=input("Name: ")
 
             print("Adieu, adieu, to ", user_input)
-
+        name=inflect(user_input)
         except ValueError
-            print ("Adieu, adieu, to ",)
+            print ("Adieu, adieu, to ", name)
