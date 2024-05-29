8ccef962aa68410c63a532c90b5052c9e4ff06ee
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-29 03:29:47 +0000

    Wed, May 29, 2024, 11:29 AM +08:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -9,13 +9,14 @@ def figlet():
     figlet = Figlet()
     user_input=input("Input: ")
 
-    if len(sys.argv)==1 or :
-        sys.exit()
+    if len(sys.argv)==2 or >3:
+        sys.exit("Invalid usage")
 
-    elif len(sys.argv)==0:
-    output=random.(user_input)
-    print(f"Output: {figlet.renderText(output)}")
+    elif len(sys.argv)==1:
+        output=random.(user_input)
+        print(f"Output: {output}")
 
-    elif
+    elif len(sys.argv)==3:
+        print(f"Output: {figlet.renderText(output)")
 
 main()
