0106d212c9f1428bff4a048284745f070ee92715
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-30 02:27:05 +0000

    Thu, May 30, 2024, 10:27 AM +08:00

--- game.py
+++ game.py
@@ -3,5 +3,7 @@ from random import randint
 #pick a random number
 number=random.randint(1,n)
 
-user_input=int(input("Level: "))
+user_input=input("Level: ")
+if user_input>number:
+    print("Too small!")
 
