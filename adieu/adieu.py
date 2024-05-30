741a98cac3cde4c4d86ad7adc8198b149b6a799b
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-30 02:01:35 +0000

    Thu, May 30, 2024, 10:01 AM +08:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -6,8 +6,7 @@ names = []
 try:
     while True:
         name = input("Name: ")
-        if name:
-            names.append(name)
+        names.append(name)
 
 except EOFError:
     print(f"\nAdieu, adieu, to {p.join(names)}")
