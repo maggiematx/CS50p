a8017ba1979bbed99bfa53c9f1dfdb4b6360dc0e
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-30 10:27:06 +0000

    Thu, May 30, 2024, 6:27 PM +08:00

---
+++ professor/professor.py
@@ -0,0 +1,22 @@
+import random
+
+
+def main():
+
+
+
+def get_level():
+    user_input=input["Level: "]
+    while True:
+        for n in range [1:3]:
+            continue
+        else:
+            break
+
+
+def generate_integer(level):
+    ...
+
+
+if __name__ == "__main__":
+    main()
