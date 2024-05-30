7c59866052716a0d37a19efd9743300901e68929
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-30 01:58:56 +0000

    Thu, May 30, 2024, 9:58 AM +08:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -1,23 +1,13 @@
 import inflect
 
 
+p = inflect.engine()
+names = []
+try:
+    while True:
+        name = input("Name: ")
+        if name:
+            names.append(name)
 
-def main():
-    names = get_names()
-
-
-def get_names():
-    p = inflect.engine()
-    names = []
-    try:
-        while True:
-            name = input("Name: ")
-            if name:
-                names.append(name)
-
-    except EOFError:
-        print(f"\nAdieu, adieu, to {p.join(names)}")
-
-
-if __name__ == "__main__":
-    main()
+except EOFError:
+    print(f"\nAdieu, adieu, to {p.join(names)}")
