3fdc5ba529eabfdc3770bbe9ea963d77fbf6de6f
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-30 02:29:50 +0000

    Thu, May 30, 2024, 10:29 AM +08:00

--- game.py
+++ game.py
@@ -5,5 +5,9 @@ number=random.randint(1,n)
 
 user_input=input("Level: ")
 if user_input>number:
-    print("Too small!")
+    print("Too large!")
+elif user_input<number:
+    print("Too smalle!")
+else:
+    print("Just right!")
 
