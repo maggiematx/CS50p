280a6b09a511122118bd9818f0d0f37c886cb9f5
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-05-30 02:34:52 +0000

    Thu, May 30, 2024, 10:34 AM +08:00

--- game.py
+++ game.py
@@ -3,13 +3,20 @@ from random import randint
 #pick a random number
 number=random.randint(1,n)
 
-user_input=input("Level: ")
-if user_input != int
-    pass
-if user_input>number:
-    print("Too large!")
-elif user_input<number:
-    print("Too smalle!")
-else:
-    print("Just right!")
+
+while True:
+    user_input=input("Level: ")
+    print("Guess: ", user_input)
+    
+    if user_input != int
+        pass
+
+
+    if user_input>number:
+        print("Too large!")
+
+    elif user_input<number:
+        print("Too smalle!")
+    else:
+        print("Just right!")
 
