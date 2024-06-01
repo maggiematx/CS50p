9631f25013f0b7ba1542accf74e56a0d4dbe8236
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-06-01 10:11:25 +0000

    Sat, Jun 1, 2024, 6:11 PM +08:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -2,28 +2,31 @@ import requests
 import sys
 import json
 
+
 def main():
 
-    if len(sys.argv)==1:
+    if len(sys.argv) == 1:
         sys.exit("Missing command-line argument")
 
     try:
-        amount=float(sys.argv[1])
+        amount = float(sys.argv[1])
     except ValueError:
         sys.exit("Command-line argument is not a number")
 
-    price=get_price()
-    cost=amount*price
+    price = get_price()
+    cost = amount * price
     print(f"${cost:,.4f}")
+
 
 def get_price():
     try:
-        response=requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+        response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
         response.raise_for_status()
-        o=response.json()
+        o = response.json()
         return o["bpi"]["USD"]["rate_float"]
 
     except requests.RequestException:
         pass
+
 
 main()
