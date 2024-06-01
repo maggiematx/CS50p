957f298b671a67f43200a84e3f533898ec841145
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-06-01 09:07:09 +0000

    Sat, Jun 1, 2024, 5:07 PM +08:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -2,17 +2,23 @@ import requests
 import sys
 import json
 
-if len(sys.argv)==1:
-    sys.exit("Missing command-line argument")
-#if input cannot be converted to a float
-elif
-    sys.exit("Command-line argument is not a number")
-else:
-    print(f"${amount:,.4f}")
+def main():
+    get_amount():
+    if len(sys.argv)==1:
+        sys.exit("Missing command-line argument")
+    #if input cannot be converted to a float
+    elif
+        sys.exit("Command-line argument is not a number")
+    else:
+        print(f"${amount:,.4f}")
 
-response=requests.get("https://api.coindesk.com/v1/bpi/currentprice.json" + sys.argv[1])
+    response=requests.get("https://api.coindesk.com/v1/bpi/currentprice.json" + sys.argv[1])
 
-def amount:
+    o=resopnse.json()
+    for result in o["results"]:
+        print
+
+def get_amount():
 
 
 
