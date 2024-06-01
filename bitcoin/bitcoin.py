3b9b2595099d4803f029c27c118d2003f9bb9cd2
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-06-01 09:57:33 +0000

    Sat, Jun 1, 2024, 5:57 PM +08:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -12,19 +12,13 @@ def main():
     except ValueError:
         sys.exit("Command-line argument is not a number")
 
-    get_price()
-    get_cost()
+    price=get_price()
+    cost=amount*price
     print(f"${cost:,.4f}")
-
-      amount=get_amount(sys.argv[1])
 
 get_price():
     try:
         response=requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
-
-def get_amount():
-    get_amount=result * (sys.argv[1])
-    while true:
 
 
         o=resopnse.json()
