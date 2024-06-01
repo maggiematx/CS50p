01ef3af943a502095bd460776551f2520ba08fa4
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-06-01 09:05:01 +0000

    Sat, Jun 1, 2024, 5:05 PM +08:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -4,12 +4,13 @@ import json
 
 if len(sys.argv)==1:
     sys.exit("Missing command-line argument")
+#if input cannot be converted to a float
 elif
     sys.exit("Command-line argument is not a number")
 else:
     print(f"${amount:,.4f}")
 
-response=requests.get()
+response=requests.get("https://api.coindesk.com/v1/bpi/currentprice.json" + sys.argv[1])
 
 def amount:
 
