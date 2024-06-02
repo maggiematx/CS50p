ffcf4d5529b2699205aef77002f5b9a09e16c127
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-06-02 01:45:02 +0000

    Sun, Jun 2, 2024, 9:45 AM +08:00

--- test2.py
+++ test2.py
@@ -1,13 +1,13 @@
-from test import square
+from calculator import square
+
 
 def main():
     test_square()
 
+
 def test_square():
-    if square(2) != 4:
-        print("2 squared was not 4!")
-    if square(3)!=9:
-        print("3 squared was not 9!")
+    assert square(2) == 4
+    assert square(3) == 9
 
 
 if __name__ == "__main__":
