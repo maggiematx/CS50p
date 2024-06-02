791672b81149fafd3d76fc35fb9ca06cc4d1faac
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-06-02 01:48:02 +0000

    Sun, Jun 2, 2024, 9:48 AM +08:00

--- test2.py
+++ test2.py
@@ -1,13 +1,29 @@
-from calculator import square
+from test import square
 
 
 def main():
     test_square()
-
-
 def test_square():
-    assert square(2) == 4
-    assert square(3) == 9
+    try:
+        assert square(2) == 4
+    except AssertionError:
+        print("2 squared is not 4")
+    try:
+        assert square(3) == 9
+    except AssertionError:
+        print("3 squared is not 9")
+    try:
+        assert square(-2) == 4
+    except AssertionError:
+        print("-2 squared is not 4")
+    try:
+        assert square(-3) == 9
+    except AssertionError:
+        print("-3 squared is not 9")
+    try:
+        assert square(0) == 0
+    except AssertionError:
+        print("0 squared is not 0")
 
 
 if __name__ == "__main__":
