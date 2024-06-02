f216cf8db7e8034247f68a58f0c11d00e8d85dba
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-06-02 01:36:40 +0000

    Sun, Jun 2, 2024, 9:36 AM +08:00

--- test.py
+++ test.py
@@ -1,12 +1,11 @@
-import json
-import requests
-import sys
+def main():
+    x = int(input("What's x? "))
+    print("x squared is", square(x))
 
-if len(sys.argv) != 2:
-    sys.exit()
 
-response = requests.get("https://itunes.apple.com/search?entity=song&limit=50&term=" + sys.argv[1])
+def square(n):
+    return n * n
 
-o = response.json()
-for result in o["results"]:
-    print(result["trackName"])
+
+if __name__ == "__main__":
+    main()
