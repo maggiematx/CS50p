060567e3564e6fc87a4dc7e6957e1be86bdb2143
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-06-03 08:32:53 +0000

    Mon, Jun 3, 2024, 4:32 PM +08:00

--- test.py
+++ test.py
@@ -1,10 +1,3 @@
-def main():
-    name=input("what's your name")
-    hello()
-
-
-def hello(to="world"):
-    print("hello", to)
-
-if __name__ == "__main__":
-    main()
+user_greeting = "  Hello, World!   "
+greeting = user_greeting.lower().replace("", "").split()
+print(greeting)
