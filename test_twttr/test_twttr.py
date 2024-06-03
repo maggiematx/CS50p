d58e454b52cbe6b53d930dc39d8fe111bea212c9
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-06-03 01:46:04 +0000

    Mon, Jun 3, 2024, 9:46 AM +08:00

--- test_twttr/test_twttr.py
+++ test_twttr/test_twttr.py
@@ -1,5 +1,11 @@
 def main():
     user_input=input("")
     print(shortened(user_input))
-def shortened():
-    
+def shortened(text):
+    v=["a","e","i","o","u"]
+    nv=""
+    for i in text:
+        if i.lower() not in v:
+            print nv+i
+    return nv
+
