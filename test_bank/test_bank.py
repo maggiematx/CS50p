cf883073e877d71e92c9b34d51d47a1e741ce1a3
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-06-03 03:22:30 +0000

    Mon, Jun 3, 2024, 11:22 AM +08:00

--- test_bank/test_bank.py
+++ test_bank/test_bank.py
@@ -4,5 +4,18 @@ def test_default():
     assert hello() == "hello, world"
 
 
-def test_argument():
-    assert hello("David") == "hello, David"
+def test_basic_argument():
+    assert value("hello world") == 0
+    assert value("hello")==0
+    assert value("how are you?")==20
+
+def test_number():
+    assert value(123)==100
+
+def test_cap():
+    assert value(" hi ")==20
+
+def test_special():
+    assert value("@#4")==100
+
+
