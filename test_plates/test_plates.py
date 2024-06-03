b77802dc49df861aad6c18d51022137b0bf1279c
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-06-03 09:20:02 +0000

    Mon, Jun 3, 2024, 5:20 PM +08:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -1,4 +1,16 @@
 from plates import is_valid
 
+def test_basic_argument():
+    assert value("CS50") == Valid
+
 def test_num():
-    assert value(" 88 ")
+    assert value(" 88 ") == Invalid
+
+def test_special_char():
+    assert value("#$ma") == Invalid
+
+
+
+
+
+
