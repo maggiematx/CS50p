56b587f5c5b99a909402efdf89ee678c50c91bdd
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-06-03 03:31:37 +0000

    Mon, Jun 3, 2024, 11:31 AM +08:00

--- test_bank/test_bank.py
+++ test_bank/test_bank.py
@@ -5,11 +5,10 @@ def test_basic_argument():
     assert value("hello")==0
     assert value("how are you?")==20
 
-def test_number():
-    assert value(123)==100
+
 
 def test_cap():
-    assert value(" hi ")==20
+    assert value(" HI ")==20
 
 def test_special():
     assert value("@#4")==100
