4169154fa189db865bef2e03c67604c9efb01f8d
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-06-03 10:18:17 +0000

    Mon, Jun 3, 2024, 6:18 PM +08:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -2,24 +2,27 @@ from plates import is_valid
 
 def test_basic_argument():
     assert is_valid("CS50") == True
-    assert is_valid("CS")== True
+    assert is_valid("CS") == True
     assert is_valid("C50") == False
 
 def test_len():
-    assert is_valid("CS50909")==False
-    assert is_valid("CSCSCS")==True
+    assert is_valid("CS50909") == False
+    assert is_valid("MAMAMAMA") == False
 
 def test_num():
-    assert is_valid(" 88") == False
-    assert is_valid("50CS")==False
-    assert is_valid("CS0505")==False
-    assert is_valid("CS05")==False
-    assert is_valid("00")==False
+    assert is_valid("88") == False
+    assert is_valid("50CS") == False
+    assert is_valid("CS0505") == False
 
 def test_special_char():
     assert is_valid("#$ma") == False
-    assert is_valid("CS50#$")== False
+    assert is_valid("CS50#$") == False
 
+def test_edge_cases():
+    assert is_valid("HI") == True
+    assert is_valid("H1") == False
+    assert is_valid("H12") == False
+    assert is_valid("HI1234") == True
 
 
 
