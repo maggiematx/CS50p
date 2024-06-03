03680c7bcaa8d122dffa179b9d5c2c7a39ceb6d8
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-06-03 02:49:10 +0000

    Mon, Jun 3, 2024, 10:49 AM +08:00

--- test_bank/bank.py
+++ test_bank/bank.py
@@ -6,7 +6,7 @@ def main():
 def value(greeting):
     if greeting.startswith ("hello"):
         return 0
-    elif greeting.startswith ("h"), and != "hello":
+    elif greeting.startswith ("h") and greeting != "hello":
         return 20
     else:
         return 100
