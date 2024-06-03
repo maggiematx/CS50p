cb3fd21c1fa5081a67bf8751bc1468ef3b92b213
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-06-03 09:28:26 +0000

    Mon, Jun 3, 2024, 5:28 PM +08:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -2,13 +2,21 @@ from plates import is_valid
 
 def test_basic_argument():
     assert is_valid("CS50") == True
+    assert is_valid("CS")== True
 
-def test_len
+def test_len():
+    assert is_valid("CS50909")==False
+    assert is_valid("MAMAMAMA")==False
+
 def test_num():
     assert is_valid(" 88 ") == False
+    assert is_valid("CS0505")==False
 
 def test_special_char():
     assert is_valid("#$ma") == False
+    assert is_valid("CS50#$)== False
+
+
 
 
 
