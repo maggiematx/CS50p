9e3188b5aa704b97b3981efdc0e929337252bb86
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-06-03 02:45:31 +0000

    Mon, Jun 3, 2024, 10:45 AM +08:00

--- test_bank/bank.py
+++ test_bank/bank.py
@@ -1,10 +1,15 @@
 def main():
-    greeting=input("Greeting: ")
+    greeting=input("Greeting: ").lower().strip("")
     print(value(greeting))
 
 
 def value(greeting):
-    
+    if greeting startswith "hello":
+        return 0
+    elif greeting startswith "h", and != "hello":
+        return 20
+    else:
+        return 100
 
 
 if __name__ == "__main__":
