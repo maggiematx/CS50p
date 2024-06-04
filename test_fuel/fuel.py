c69b5d0cce016bb1452b9086105b9a791a60daa9
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-06-04 03:37:03 +0000

    Tue, Jun 4, 2024, 11:37 AM +08:00

--- test_fuel/fuel.py
+++ test_fuel/fuel.py
@@ -5,4 +5,17 @@ def main():
 
 
 def convert(fraction):
-    fraction= [.0f]
+    fraction= [.0f]*100%
+
+while true:
+    try:
+        if fraction =<1%:
+            return E
+        elif fraction >= 99%:
+            return F
+        elif x != int or y != int, or x>y:
+            pass
+    except ZeroDivisonError:
+        pass
+
+
