582011e7c92976d182f9be5235e48277187c48f1
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-06-04 01:18:18 +0000

    Tue, Jun 4, 2024, 9:18 AM +08:00

--- test.py
+++ test.py
@@ -1,2 +1,7 @@
-
-def__init__
+bankaccount Product:
+ def__init__(self,name,account number,balance)
+    self.name=name
+    self.account=account
+    self.number=number
+    self.balance=balance
+def (self):
