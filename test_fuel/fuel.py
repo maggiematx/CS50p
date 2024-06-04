3c17819ae8cb7af05e09e9c1ffdf28c8a917ffec
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-06-04 08:22:47 +0000

    Tue, Jun 4, 2024, 4:22 PM +08:00

--- test_fuel/fuel.py
+++ test_fuel/fuel.py
@@ -1,26 +1,30 @@
 def main():
+     while True:
+        try:
 
-while True:
-    try:
-        fraction= input("Fraction: ")
-        x, y = map(int,fraction.split('/'))
+            fraction= input("Fraction: ")
+            x, y = map(int,fraction.split('/'))
 
-        if x > y:
-            raise ValueError
+            if x > y:
+                raise ValueError
+
+            if 99 <= percentage <= 100:
+                print('F')
+            elif percentage<=1:
+                print('E')
+            else:
+                print(f"{percentage:.0f}%")
+                break
+
+        except (ValueError, ZeroDivisionError):
+            pass
+
+
+
+
 
 def convert(fractioin):
-     
+
 def guage(percentage):
         percentage = (x / y) * 100
-
-        if 99 <= percentage <= 100:
-            print('F')
-        elif percentage<=1:
-            print('E')
-        else:
-            print(f"{percentage:.0f}%")
-        break
-
-    except (ValueError, ZeroDivisionError):
-        pass
 
