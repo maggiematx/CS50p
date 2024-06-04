1b4b1ae15e85c4f12b071fa31e85a22aadbf98fc
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-06-04 11:06:40 +0000

    Tue, Jun 4, 2024, 7:06 PM +08:00

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -1,31 +1,21 @@
 from fuel import convert and gauge
 
-def test_basic_argument():
-    assert convert("1/2") == 50%
-    assert gauge("50%) == .5
-
-def test_len():
-    assert is_valid("CS50909") == False
-    assert is_valid("MAMAMAMA") == False
+def test_convert():
+    assert convert("1/2") == 50
+    assert conver("0/1") == 0
+    assert convert("6/6") == 0
 
 
-def test_special_char():
-    assert convert("#$ma") == 
-    assert is_valid("CS50#$") == False
-
-def test_edge_cases():
-    assert is_valid("HI") == True
-    assert is_valid("H1") == False
-    assert is_valid("H12") == False
-    assert is_valid("HI1234") == True
-
-def test_number_placement():
-    assert is_valid("CS50A") == False
-    assert is_valid("CS05") == False
-    assert is_valid("CS5") == True
+def test_gauge():
+    assert gauge("50%") == 50%
+    assert gauge("1%")==E
+    assert guage("99%")==F
 
 
+def test_value_error():
+    with pytest.raises(ValueError):
+        convert("x/y)
 
-
-
-
+def test_zero_divison_error():
+    with pytest.raises(ZeroDivisonError):
+        convert("5/0")
