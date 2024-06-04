e9047d6da7d3d3abcd9793e87f00b3e9a6a7ae99
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-06-04 11:20:13 +0000

    Tue, Jun 4, 2024, 7:20 PM +08:00

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -4,13 +4,13 @@ import pytest
 def test_convert():
     assert convert("1/2") == 50
     assert convert("0/1") == 0
-    assert convert("6/6") == 1
+    assert convert("6/6") == 100
 
 
 def test_gauge():
     assert gauge(50) == "50%"
     assert gauge(1)=="E"
-    assert guage(99)=="F"
+    assert gauge(99.5)=="F"
 
 
 def test_value_error():
