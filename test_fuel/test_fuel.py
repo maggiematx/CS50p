9d61d5171451b014c365cc3bafbd85b81dcfe172
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-06-04 11:11:23 +0000

    Tue, Jun 4, 2024, 7:11 PM +08:00

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -1,20 +1,21 @@
-from fuel import convert and gauge
+from fuel import convert, gauge
+import pytest
 
 def test_convert():
     assert convert("1/2") == 50
-    assert conver("0/1") == 0
+    assert convert("0/1") == 0
     assert convert("6/6") == 0
 
 
 def test_gauge():
-    assert gauge("50%") == 50%
+    assert gauge("50") == "50%"
     assert gauge("1%")==E
     assert guage("99%")==F
 
 
 def test_value_error():
     with pytest.raises(ValueError):
-        convert("x/y)
+        convert("x/y")
 
 def test_zero_divison_error():
     with pytest.raises(ZeroDivisonError):
