44b5988fe4a51a6f6ea91af8a7ac6fe4dae463e4
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-06-04 01:22:40 +0000

    Tue, Jun 4, 2024, 9:22 AM +08:00

--- test.py
+++ test.py
@@ -1,12 +1,3 @@
-bankaccount Product:
- def__init__(self,name,account number,balance)
-    self.name=name
-    self.account=account
-    self.number=number
-    self.balance=balance
-def_display_balance(self):
-obj1=Product("Maggie", 12345, $500)
-obj1.getbalace()
 
 class BankAccount:
     def __init__(self, name, account_number, balance=0):
@@ -19,7 +10,7 @@ class BankAccount:
 
 # Example usage:
 def main():
-    # Create an instance of the BankAccount class
+ 
     obj1 = BankAccount("Maggie", 12345, 500)
 
     # Display the balance
