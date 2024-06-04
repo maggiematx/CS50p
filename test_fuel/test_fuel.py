118352cdf9d1f75873d4ec4b513fed66f3b2a620
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-06-04 10:59:27 +0000

    Tue, Jun 4, 2024, 6:59 PM +08:00

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -8,15 +8,9 @@ def test_len():
     assert is_valid("CS50909") == False
     assert is_valid("MAMAMAMA") == False
 
-def test_num():
-    assert is_valid("88") == False
-    assert is_valid("50CS") == False
-    assert is_valid("CS0505") == False
-    assert is_valid("CS50") == True  # valid example
-    assert is_valid("C50") == False  # invalid, not enough initial letters
 
 def test_special_char():
-    assert is_valid("#$ma") == False
+    assert convert("#$ma") == 
     assert is_valid("CS50#$") == False
 
 def test_edge_cases():
