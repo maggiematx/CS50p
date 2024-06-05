f8736334dff39c66c459e311310cb75d5c3d0cda
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-06-05 01:21:26 +0000

    Wed, Jun 5, 2024, 9:21 AM +08:00

--- test2.py
+++ test2.py
@@ -2,4 +2,11 @@ def main():
     while True:
         try:
             user_input=input("Fraction: ")
-            
+            fraction=convert(user_input)
+            percentage=gauge(fraction)
+            print(gauge(convert(user_input)))
+        except:
+            raise  ValueError
+
+def convert(fraction):
+    
