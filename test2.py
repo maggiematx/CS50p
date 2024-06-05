93d84733735221b79d0a1aa68c8437eea8705d2f
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-06-05 03:11:14 +0000

    Wed, Jun 5, 2024, 11:11 AM +08:00

--- test2.py
+++ test2.py
@@ -1,3 +1,15 @@
-name=input("name: ")
-house=input("house: ")
-print(f"{name}from{house}")
+def main():
+    student = get_student()
+    if student[0] == "Padma":
+        student[1] = "Ravenclaw"
+    print(f"{student[0]} from {student[1]}")
+
+
+def get_student():
+    name = input("Name: ")
+    house = input("House: ")
+    return name, house
+
+
+if __name__ == "__main__":
+    main()
