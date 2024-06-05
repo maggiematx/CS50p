31f906faba80ae4dde401cb638d46ccd469c1e4b
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-06-05 02:47:10 +0000

    Wed, Jun 5, 2024, 10:47 AM +08:00

--- test2.py
+++ test2.py
@@ -1,29 +1,3 @@
-def main():
-    while True:
-        try:
-            user_input=input("Fraction: ")
-            fraction=convert(user_input)
-            percentage=gauge(fraction)
-            print(gauge(convert(user_input)))
-        except (ValueError, ZeroDivisonError)
-
-def convert(fraction):
-    x, y= map(int,fraction.split("/"))
-    while True:
-        try:
-            if x > y:
-                raise ValueError
-            elif y==0:
-                raise ZeroDivisonError
-            else:
-                percentage=x/y*100
-        except:
-
-def gauge(percentage):
-    if percentage<=1:
-        return "E"
-    elif percentage=>99:
-        return "F"
-    else:
-        return 
-
+name=input("name: ")
+house=input("house: ")
+print(f"{name}from{house}")
