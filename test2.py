3c1c8da87d8fcbae453e489636d7b752e0412faa
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-06-05 01:18:29 +0000

    Wed, Jun 5, 2024, 9:18 AM +08:00

--- test2.py
+++ test2.py
@@ -1,10 +1,5 @@
-from test import square
-
-
-def test_assert():
-    assert square(2) == 4
-    assert square(3) == 9
-    assert square(-2) == 4
-    assert square(-3) == 9
-    assert square(0) == 0
-    assert square(cat)=="no valid"
+def main():
+    while True:
+        try:
+            user_input=input("Fraction: ")
+            
