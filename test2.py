8b4202a885df126aaec09bf09dbed31b79cb02fa
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-06-06 15:10:23 +0000

    Thu, Jun 6, 2024, 11:10 PM +08:00

--- test2.py
+++ test2.py
@@ -6,6 +6,6 @@ class Student:
     def do_hobby(self):
                 if self.hobby=="Tennis":
                            print(self.name,"plays tennis")
-obj1 = Student("jackson","Tennis")
+obj1 = Student("jackson","Tenis")
 print(obj1.name)
 obj1.do_hobby()
