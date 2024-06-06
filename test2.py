b1b292d2047f818299803beee586644ab4eea190
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-06-06 16:06:08 +0000

    Fri, Jun 7, 2024, 12:06 AM +08:00

--- test2.py
+++ test2.py
@@ -5,19 +5,16 @@ class Product:
                  self.price=float(price)
                  self.discount_percentage=discount_percentage/100
 
-    def get_discount_amount(self):
-                discount=self.price*self.discount_percentage
-                print("The discount is ", discount)
-                return discount
 
     def get_discount_price(self):
-                discount=self.get_discount_amount()
+                discount=self.price*self.discount_percentage
+                print("The discount is ", discount)
                 discount_price=self.price-discount
                 print("The discount price is ", discount_price)
 
 obj1 = Product("Stanely 13 Ounce Wood Hammer","500",10)
 obj2=Product('National Hardware 3/4" Wire Nails',5.06, 0)
 
-obj1.get_discount_amount()
+
 
 obj1.get_discount_price()
