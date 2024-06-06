7e7ea54df27141ebfa3d56b0fbebe9dbb2f6cba4
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-06-06 15:39:10 +0000

    Thu, Jun 6, 2024, 11:39 PM +08:00

--- test2.py
+++ test2.py
@@ -3,14 +3,14 @@ class Product:
     def __init__(self,name,price,discount_percentage):
                  self.name=name
                  self.price=price
-                 self.discount_percentage=percentage
+                 self.discount_percentage=discount_percentage
 
     def get_discount_amount(self):
                 if self.hobby=="Tennis":
                            print(self.name,"plays tennis")
     def get_discount_price(self):
 
-obj1 = Product("Stanely 13 Ounce Wood Hammer","12.99")
-obj2=Product
+obj1 = Product("Stanely 13 Ounce Wood Hammer","12.99",62)
+obj2=Product('National Hardware 3/4" Wire Nails',5.06 0)
 print(obj1.name)
 obj1.do_hobby()
