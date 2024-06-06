a37e3ae2483264e28232ad3cf0b49b9db001239b
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-06-06 15:34:11 +0000

    Thu, Jun 6, 2024, 11:34 PM +08:00

--- test2.py
+++ test2.py
@@ -1,12 +1,15 @@
 class Product:
 
-    def __init__(self,name,price,discount_percentage_rate):
+    def __init__(self,name,price,discount_percentage):
                  self.name=name
                  self.price=price
+                 self.discount_percentage=percentage
 
-    def do_hobby(self):
+    def get_discount_amount(self):
                 if self.hobby=="Tennis":
                            print(self.name,"plays tennis")
+    def get_discount_price(self):
+            
 obj1 = Student("jackson","Tenis")
 print(obj1.name)
 obj1.do_hobby()
