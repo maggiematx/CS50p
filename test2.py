93584689598c2ca74f0d79adeef264ba41a28c02
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-06-06 02:30:49 +0000

    Thu, Jun 6, 2024, 10:30 AM +08:00

--- test2.py
+++ test2.py
@@ -3,10 +3,9 @@ def main():
     print(f"{student['name']} from {student['house']}")
 
 def get_student():
-    student={}
     student["name"]=input("name: ")
     student["house"]=input("house: ")
-    return student
+    return {"name": name, "house":house}
 
 if __name__ == "__main__":
     main()
