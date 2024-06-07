3f561da0d3c384baff6e36c0d88c536d6e5bd4af
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-06-07 02:22:33 +0000

    Fri, Jun 7, 2024, 10:22 AM +08:00

--- test2.py
+++ test2.py
@@ -3,18 +3,10 @@ class BankAccount:
     def __init__(self,name,account_number,balance):
                  self.name=name
                  self.account_number=account_number
-                 self.balance=blance
+                 self.balance=balance
 
-
-    def get_discount_price(self):
-                discount=self.price*self.discount_percentage
-                print("The discount is ", discount)
-                discount_price=self.price-discount
-                print("The discount price is ", discount_price)
-
-obj1 = Product("Stanely 13 Ounce Wood Hammer","500",10)
-obj2=Product('National Hardware 3/4" Wire Nails',5.06, 0)
+obj1 =  BankAccount("Maggie", "12345", "500")
+print("The account balance for f"{self.name}" is $f"{self.balance}")
 
 
 
-obj1.get_discount_price()
