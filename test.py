73b2cd47e180458793978de8eef2d12cf69a8631
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-06-08 09:51:16 +0000

    Sat, Jun 8, 2024, 5:51 PM +08:00

--- test.py
+++ test.py
@@ -8,10 +8,7 @@ class Time:
         self.seconds=seconds
 
     def display_time(self):
-        hours="5"
-        minutes="33"
-        seconds="45"
-        return Time(hours, minutes, seconds)
+
 
 def main():
     obj1 = Time()
@@ -19,8 +16,12 @@ def main():
     formatted_datetime = current_ime.strftime("%H:%M:%S")
     Print("The current time is display_time(formatted_datetime)")
 
-def time():
-    
+def Time():
+    hours=formatted_datetime[H]
+    minutes=formatted_datetime[M]
+    seconds=formatted_datetime[S]
+    return Time(hours, minutes, seconds)
+
 
 if __name__== "__main__":
     main()
