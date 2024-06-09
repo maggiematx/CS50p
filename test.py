6b993b52e52b5ae1f1ac29cd2bf40b5f07b71e10
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-06-09 22:16:01 +0000

    Mon, Jun 10, 2024, 6:16 AM +08:00

--- test.py
+++ test.py
@@ -1,36 +1,19 @@
 class Student:
     def __init__(self, name, house):
+        if not name:
+            raise ValueError("Invalid name")
+        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("Invalid house")
         self.name = name
         self.house = house
 
     def __str__(self):
         return f"{self.name} from {self.house}"
 
-    # Getter for name
-    @property
-    def name(self):
-        return self._name
-
-    # Setter for name
-    @name.setter
-    def name(self, name):
-        if not name:
-            raise ValueError("Invalid name")
-        self._name = name
-
-    @property
-    def house(self):
-        return self._house
-
-    @house.setter
-    def house(self, house):
-        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
-            raise ValueError("Invalid house")
-        self._house = house
-
 
 def main():
     student = get_student()
+    student.house = "Number Four, Privet Drive"
     print(student)
 
 
