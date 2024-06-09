9c1351458623329d773e19d89779a2f6b3119122
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-06-09 22:22:21 +0000

    Mon, Jun 10, 2024, 6:22 AM +08:00

--- test.py
+++ test.py
@@ -1,26 +1,17 @@
-class Student:
-    def __init__(self, name, house):
-        if not name:
-            raise ValueError("Invalid name")
-        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
-            raise ValueError("Invalid house")
-        self.name = name
-        self.house = house
 
-    def __str__(self):
-        return f"{self.name} from {self.house}"
 
 
 def main():
     student = get_student()
-    student.house = "Number Four, Privet Drive"
-    print(student)
+    if student[0]=="Padma":
+        student[1]=="Ravenclas"
+    print(f"{student[0]} from {student[1]}")
 
 
 def get_student():
     name = input("Name: ")
     house = input("House: ")
-    return Student(name, house)
+    return [name, house
 
 
 if __name__ == "__main__":
