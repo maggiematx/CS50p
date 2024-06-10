7ba0b9c183c7f1b8b07ad5b8f2bf57d5c9affcce
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-06-10 01:33:44 +0000

    Mon, Jun 10, 2024, 9:33 AM +08:00

--- test.py
+++ test.py
@@ -1,7 +1,13 @@
 def main():
     student=get_student()
-    if student[0]=="Padma"
-    
+    if student[0]=="Padma":
+        studnet[1]=="Ravenclaw"
+    print(f"{student[0]} from{student[1]}")
+
+def get_student():
+    name=input("Name: ")
+    house=input("House: ")
+    return name, house
 
 
 if __name__ == "__main__":
