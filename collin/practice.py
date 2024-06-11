7312d10da12ba7a2462406a693b83de3c11b3930
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-06-11 00:35:20 +0000

    Tue, Jun 11, 2024, 8:35 AM +08:00

--- collin/practice.py
+++ collin/practice.py
@@ -5,10 +5,21 @@ class Employee:
                  self.salary=salary
                  self.project_status=project_status
 
+class
+        def __init__():
+                .__init__(self, name, grade)
+
+        def specific_descrition(self):
+                print()
+
+class
 
 obj1=Employee(maggie,4000, complete)
-print(obj.name)
-print(obj1.salary)
-obj1.doproject_Status()
+obj1.
+obj1.
+
+obj2=()
+obj2.
+obj2.
 
 t.convert()
