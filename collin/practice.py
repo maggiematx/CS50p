76cad3173ac0e2cd46d0703acd89b109b8b86efd
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-06-11 03:23:26 +0000

    Tue, Jun 11, 2024, 11:23 AM +08:00

--- collin/practice.py
+++ collin/practice.py
@@ -1,19 +1,19 @@
 class Employee:
-        def __init__(self,name,salary,project_satus):
+        def __init__(self,name,salary,project_status):
                  self.name=name
                  self.salary=salary
                  self.project_status=project_status
 
-        def print_time(self):
-                print("Time is: ", self.hours, ":", self.minutes,":",self.seconds)
+        def final_status(self):
+                if self.project_status=="complete":
+                        print(f"{self.name} receives 10% pay rise!")
+                else:
+                        print(f"{self.name} is fired!")
 
-        def display_seconds(self):
-                total_seconds=self.hours*3600+self.minutes*60+self.seconds
-                print("Time in seconds is:", total_seconds)
 
-
-obj1=Time(5,3,23)
-obj1.print_time()
-obj1.display_seconds()
+obj1=Employee("Maggie", "$4000", "complete")
+obj2=Employee("kevin", "$3000", "incomplete")
+obj1.final_status()
+obj2.final_status()
 
 
