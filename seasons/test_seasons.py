4df8bc0f0814f877a48ebe7b4c9aa99bbcf10276
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-06-15 02:55:31 +0000

    Sat, Jun 15, 2024, 10:55 AM +08:00

---
+++ seasons/test_seasons.py
@@ -0,0 +1,22 @@
+from seasons import get_birthday, calculate_age_in_timedelta,convert_minutes_to_words
+import pytest
+
+def test_get_birthday():
+    assert convert("1/2") == 50
+    assert convert("0/1") == 0
+    assert convert("6/6") == 100
+
+
+def test_gauge():
+    assert gauge(50) == "50%"
+    assert gauge(1)=="E"
+    assert gauge(99)=="F"
+
+
+def test_value_error():
+    with pytest.raises(ValueError):
+        convert("x/y")
+
+def test_zero_divison_error():
+    with pytest.raises(ZeroDivisionError):
+        convert("5/0")
