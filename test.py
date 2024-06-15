a4d7401c510cf1f8abf6918afd0e402ba2af8e8a
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-06-15 02:19:29 +0000

    Sat, Jun 15, 2024, 10:19 AM +08:00

--- test.py
+++ test.py
@@ -1,18 +1,17 @@
-from datetime import datetime, timedelta
+from datetime import datetime, date
 import inflect
+import sys
 
 class Age:
     def __init__(self, timedelta):
         self.timedelta = timedelta
 
     def display_minutes(self):
-        # Calculate the total number of minutes
-        total_seconds = self.timedelta.total_seconds()
-        total_minutes = total_seconds // 60  # Convert seconds to minutes
+        total_minutes = self.timedelta.total_seconds() / 60
         return int(total_minutes)
 
 def main():
-    birth_date = datetime.strptime("2023-04-16", "%Y-%m-%d").date()
+    birth_date = get_birthday()
     age_in_timedelta = calculate_age_in_timedelta(birth_date)
     age = Age(age_in_timedelta)
 
@@ -21,18 +20,27 @@ def main():
 
     print(f"{total_minutes_in_words} minutes")
 
+def get_birthday():
+    while True:
+        birthday_str = input("Date of Birth (YYYY-MM-DD): ")
+        try:
+            birthday = datetime.strptime(birthday_str, "%Y-%m-%d").date()
+            return birthday
+        except ValueError:
+            print("Invalid date. Please use the format YYYY-MM-DD.")
+            sys.exit(1)
+
 def calculate_age_in_timedelta(birth_date):
-    today = datetime.now().date()  # Get today's date
-    # Calculate the timedelta between today and the birth_date
+    today = datetime.now().date()
     age_in_timedelta = today - birth_date
     return age_in_timedelta
 
 def convert_minutes_to_words(total_minutes):
     p = inflect.engine()
     total_minutes_in_words = p.number_to_words(total_minutes)
-    # Replace commas and "and"
     total_minutes_in_words = total_minutes_in_words.replace(",", "").replace(" and", "")
     return total_minutes_in_words
 
 if __name__ == "__main__":
     main()
+
