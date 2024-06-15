4482f3ad9b36b593818324f5f030286272850976
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-06-15 02:42:01 +0000

    Sat, Jun 15, 2024, 10:42 AM +08:00

--- test.py
+++ test.py
@@ -1,28 +1,66 @@
-from datetime import datetime
+from datetime import date
 import inflect
 import sys
-
-class Age:
-    def __init__(self, timedelta):
-        self.timedelta = timedelta
-
-    def display_minutes(self):
-        total_minutes = self.timedelta.total_seconds() // 60
-        return total_minutes
+import re
 
 def main():
-    birth_date = get_birthday()
-    age_in_timedelta = calculate_age_in_timedelta(birth_date)
-    age = Age(age_in_timedelta)
+    inpt=input('Date of Birth: ')
+    print(get_min(inpt))
 
-    total_minutes = age.display_minutes()
-    total_minutes_in_words = convert_minutes_to_words(total_minutes)
 
-    print(f"{total_minutes_in_words} minutes")
+def get_min(inpt):
 
-def get_birthday():
-    while True:
-        birthday_str = input("
+    # gets dates from input using regex
+    if search := re.search(r'^(\d{4})-(\d{2})-(\d{2})$',inpt):
+        inpt=list(search.groups())
+    else:
+        sys.exit('Invalid date')
+
+    # converts str into int and checks valid date
+    inpt_bday = convert_and_check(inpt)
+
+    # today's date
+    today = date.today()
+
+    # bday
+    bday = date(inpt_bday[0], inpt_bday[1], inpt_bday[2])
+
+    # computes no of days
+    diff = bday - today
+    no_of_days = -int(diff.days)
+
+    # computes days into minutes
+    minutes = no_of_days * 24 * 60
+
+    # converts into words
+    inf = inflect.engine()
+    min_words = inf.number_to_words(minutes)
+
+    # remove 'and' and capitalize
+
+    min_words = min_words.replace(' and','').capitalize()
+
+    # return
+    return min_words + ' minutes'
+
+
+def convert_and_check(day):
+    # convert str to int
+    day = list(map(int, day))
+
+    # check valid date and month
+    if day[1] < 0 or day[1] > 12:
+        sys.exit('Invalid date')
+
+    elif day[2] < 0 or day[2] > 31:
+        sys.exit('Invalid date')
+
+    return day
+
+
+
+if __name__ == "__main__":
+    main()
 
 
 
