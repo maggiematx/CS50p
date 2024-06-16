c49f9f93598438ad7a428175e135b5bd6523dd92
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-06-16 08:22:32 +0000

    Sun, Jun 16, 2024, 4:22 PM +08:00

--- test.py
+++ test.py
@@ -1,20 +1,4 @@
-from datetime import date
-import inflect
-import re
+names = []
 
-p = inflect.engine()
-
-def main():
-    days_between = date.today() - check_date()
-    print (f"{p.number_to_words(days_between.days *24 * 60)}, minutes")
-
-def check_date():
-    while True:
-        birth_date = input("Enter birthdate: ").strip()
-        if re.search(r"(\d{4})-([0-1]\d)-([0-3]\d)$", birth_date):
-            return date.fromisoformat(birth_date)
-        else:
-            print("Invalid Date")
-
-if __name__ == "__main__":
-    main()
+for _ in range(3):
+    names.append(input("What's your name?" ))
