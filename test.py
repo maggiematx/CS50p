502ca35fd7081c1b58548441100c328433999bc3
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-06-17 08:33:46 +0000

    Mon, Jun 17, 2024, 4:33 PM +08:00

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ students = []
 
 with open("students.csv") as file:
     reader = csv.reader(file)
-    for row in reader:
+    for name, home in reader:
         students.append({"name": name, "home": home})
 
 for student in sorted(students, key=lambda student: student["name"]):
