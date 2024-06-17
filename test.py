ecbde8af615212ea3e96555679dbc931ba798f6b
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-06-17 08:37:50 +0000

    Mon, Jun 17, 2024, 4:37 PM +08:00

--- test.py
+++ test.py
@@ -4,8 +4,8 @@ students = []
 
 with open("students.csv") as file:
     reader = csv.reader(file)
-    for name, home in reader:
-        students.append({"name": name, "home": home})
+    for row in reader:
+        students.append({"name": row[0], "home": row[1]})
 
 for student in sorted(students, key=lambda student: student["name"]):
     print(f"{student['name']} is from {student['home']}")
