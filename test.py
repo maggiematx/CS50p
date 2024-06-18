cb9649c05b1cccd6f27977cd01d3ad74d6c02b92
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-06-18 01:03:33 +0000

    Tue, Jun 18, 2024, 9:03 AM +08:00

--- test.py
+++ test.py
@@ -2,9 +2,11 @@ class Super:
     def__init__(self):
     self.supVar=1
 class Sub(Super):
-    subVar=2
+    def__init__(self):
+    self.supVar=12
 
 obj=Sub()
+
 
 print (obj.subVar)
 print(obj.supVar)
