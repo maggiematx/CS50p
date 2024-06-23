234c5e1cdf6bb6f8070293c174c9920154262dcc
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-06-23 04:50:15 +0000

    Sat, Jun 22, 2024, 11:50 PM -05:00

--- collin/.py
+++ collin/.py
@@ -1,12 +1,7 @@
-class ExampleClass:
-             counter = 0
-             def __init__(self,first):
-                        self.first = first
-
-o1 = ExampleClass(1)
-o2= ExampleClass(2)
-o3 = ExampleClass(4)
-
-print(o1.__dict__, o1.counter)
-print(o2.__dict__, o2.counter)
-print(o3.__dict__, o3.counter)
+class Super:
+    supVar = 1
+class Sub(Super):
+    subVar = 2
+obj = Sub()
+print(obj.subVar)
+print(obj.supVar)
