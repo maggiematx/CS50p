20717b0dada9d1268c32f16cf8a12c3160dcba7a
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-06-23 05:09:21 +0000

    Sun, Jun 23, 2024, 12:09 AM -05:00

--- collin/.py
+++ collin/.py
@@ -1,7 +1,17 @@
-class Classy:
-               varia = 2
-               def method(self,var):
-                              print(self.varia, self.var)
-obj = Classy()
-obj.var = 3
-obj.method()
+class Student:
+    # Class variable
+    school_name = 'ABC School '
+    # constructor
+    def __init__(self, name, roll_no):
+        self.name = name
+        self.roll_no = roll_no
+    # Instance method
+    def show(self):
+        print('Inside instance method')
+        # access using self
+        print(self.name, self.roll_no, self.school_name)
+# create Object
+s1 = Student('Emma', 10)
+s1.show()
+# access using object reference
+print(s1.school_name)
