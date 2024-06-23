d9413f83a670c41e01c9dce45a9b81562f356221
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-06-23 05:00:04 +0000

    Sun, Jun 23, 2024, 12:00 AM -05:00

--- collin/.py
+++ collin/.py
@@ -1,11 +1,7 @@
-class Super:
-    def __init__(self,supVar):
-        self.supVar = supVar
-
-class Sub(Super):
-    def __init__(self,supVar,A):
-        super().__init__(supVar)
-        self.A = A
-obj = Sub(2,5)
-print(obj.A)
-print(obj.supVar)
+class Classy:
+               varia = 2
+               def method(self,var):
+                              print(self.varia, self.var)
+obj = Classy()
+obj.var = 3
+obj.method()
