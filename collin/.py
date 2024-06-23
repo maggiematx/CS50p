0e427a4ec5e2b31132bf3ef6b1c812c534ccba3c
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-06-23 05:44:56 +0000

    Sun, Jun 23, 2024, 12:44 AM -05:00

--- collin/.py
+++ collin/.py
@@ -1,17 +1,21 @@
 class Student:
     # Class variable
-    school_name = 'ABC School '
+    school_name = "ABC School "
+
     # constructor
     def __init__(self, name, roll_no):
         self.name = name
         self.roll_no = roll_no
+
     # Instance method
     def show(self):
-        print('Inside instance method')
+        print("Inside instance method")
         # access using self
         print(self.name, self.roll_no, self.school_name)
+
+
 # create Object
-s1 = Student('Emma', 10)
+s1 = Student("Emma", 10)
 s1.show()
 # access using object reference
 print(s1.school_name)
