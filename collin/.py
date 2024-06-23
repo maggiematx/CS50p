409296b5009947ad403fa51d51f262da92d84bf9
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-06-23 04:46:09 +0000

    Sat, Jun 22, 2024, 11:46 PM -05:00

--- collin/.py
+++ collin/.py
@@ -1,13 +1,12 @@
 class ExampleClass:
-                  def __init__(self,first):
-                                    self.first = first
-                  def set_second(self, second):
-                                     self.second = second
-o1= ExampleClass(1)
-o2 = ExampleClass(2)
+             counter = 0
+             def __init__(self,first):
+                        self.first = first
+
+o1 = ExampleClass(1)
+o2= ExampleClass(2)
 o3 = ExampleClass(4)
-o2.set_second(3)
-o3.third = 5
-print(o1.__dict__)
-print(o2.__dict__)
-print(o3.__dict__)
+
+print(o1.__dict__, o1.counter)
+print(o2.__dict__, o2.counter)
+print(o3.__dict__, o3.counter)
