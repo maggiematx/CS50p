de3fa507f8420bdbd362ae80e68257fd2d9884e3
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-06-25 00:12:01 +0000

    Mon, Jun 24, 2024, 7:12 PM -05:00

--- collin/practice.py
+++ collin/practice.py
@@ -1,21 +1,13 @@
-class Employee:
-        def __init__(self,name,salary,project_status):
-                 self.name=name
-                 self.salary=salary
-                 self.project_status=project_status
-
-        def final_status(self):
-                if self.project_status=="complete":
-                        print(f"{self.name} receives 10% pay rise!")
-                else:
-                        print(f"{self.name} is fired!")
-
-
-obj1=Employee("Maggie", "$4000", "complete")
-obj2=Employee("kevin", "$3000", "incomplete")
-obj1.final_status()
-obj2.final_status()
-# print(obj1.__dict__, obj1)
-
-
-
+class ExampleClass:
+                  def __init__(self,first):
+                                    self.first = first
+                  def set_second(self, second):
+                                     self.second = second
+o1= ExampleClass(1)
+o2 = ExampleClass(2)
+o3 = ExampleClass(4)
+o2.set_second(3)
+o3.third = 5
+print(o1.__dict__)
+print(o2.__dict__)
+print(o3.__dict__)
