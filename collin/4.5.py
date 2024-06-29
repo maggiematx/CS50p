4bd96d82cf61bc3d3a8a6dda1fedcebce3fa8dbc
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-06-29 16:01:58 +0000

    Sat, Jun 29, 2024, 11:01 AM -05:00

--- collin/4.5.py
+++ collin/4.5.py
@@ -1,17 +1,23 @@
 class A:
-
+    # parent class
     pass
 
 class B(A):
-    # Child class of A (does nothing in this example)
+    # Child class of A
     pass
 
 class C(B):
-    # Child class of A (does nothing in this example)
+    # Child class of B, which is also a child class of A
     pass
 # Create an instance of C
 c = C()
 
 print(isinstance(c, A))
 
+"""
+The output of the program is:
+True
+
+The instance c of class C is part of the inheritance hierarchy that includes class A.
+"""
 
