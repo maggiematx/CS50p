e5ec0f53052ceced07b4958a5016e49fc65a4ce6
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-06-29 15:33:01 +0000

    Sat, Jun 29, 2024, 10:33 AM -05:00

--- collin/4.4.py
+++ collin/4.4.py
@@ -30,3 +30,41 @@ print(hasattr(b,"var"))
 
 print(hasattr(B,"super"))
 
+"""
+The output of the program is:
+True
+True
+True
+True
+True
+True
+False
+
+1. hasattr(A, "__init__"):
+This checks if the class A has an __init__ method.
+Since A defines an __init__ method, this check will return True.
+
+2. hasattr(B, "__init__"):
+This checks if the class B has an __init__ method.
+Since B defines an __init__ method, this check will return True.
+
+3. hasattr(b, "__init__"):
+This checks if the instance b has an __init__ method.
+In Python, methods defined in the class (like __init__) are not directly stored in the instance; they are accessed through the class. However, hasattr will return True because b can access the __init__ method via its class.
+
+4. hasattr(b, "x"):
+This checks if the instance b has an attribute x.
+The __init__ method of B calls the __init__ method of A via super().__init__(), which sets self.x = 1. So, b does have an attribute x.
+
+5. hasattr(A, "var"):
+This checks if the class A has an attribute var.
+Since var is defined as a class attribute in A, this check will return True.
+
+6. hasattr(b, "var"):
+This checks if the instance b has an attribute var.
+The instance b inherits the class attribute var from A, so this check will return True.
+
+7. hasattr(B, "super"):
+This checks if the class B has an attribute super.
+super is a built-in function in Python and is not an attribute of the class B, so this check will return False.
+"""
