a86cb2c8e58f3424b0fc3b4fe54b069356513285
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-07-07 01:53:08 +0000

    Sat, Jul 6, 2024, 8:53 PM -05:00

--- collin/practice.py
+++ collin/practice.py
@@ -1,29 +1,15 @@
-class Level1:
-    variable_1 = 100
-    def __init__(self):
-        self.var_1 = 101
-    def fun_1(self):
-        return 102
-class Level2(Level1):
-    variable_2 = 200
-    def __init__(self):
-        super().__init__()
-        self.var_2 = 201
-    def fun_2(self):
-        return 202
-class Level3(Level2):
-    variable_3 = 300
-    def __init__(self):
-        super().__init__()
-        self.var_3 = 301
-    def fun_3(self):
-        return 302
-
-obj = Level3()
-print(obj.variable_1, obj.var_1, obj.fun_1())
-print(obj.variable_2, obj.var_2, obj.fun_2())
-print(obj.variable_3, obj.var_3, obj.fun_3())
-
-# 100, 101, 102
-# 200, 201, 202
-# 300, 301, 302
+class Left:
+    var = "L"
+    var_left = "LL"
+    def fun(self):
+        return "Left"
+class Right:
+    var = "R"
+    var_right = "RR"
+    def fun(self):
+        return "Right"
+class Sub(Left, Right):
+    pass
+obj = Sub()
+print(obj.var, obj.var_left, obj.var_right, obj.fun())
+# 
