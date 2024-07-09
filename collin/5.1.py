727dc0596cc4e966ed9575d1be5015b3409bfd99
maggiematx <143577350+maggiematx@users.noreply.github.com>
2024-07-09 00:47:56 +0000

    Mon, Jul 8, 2024, 7:47 PM -05:00

--- collin/5.1.py
+++ collin/5.1.py
@@ -1,31 +1,44 @@
+# Base class Top
 class Top:
-
+        # Method of the Top class
         def m_top(self):
-
                 print("top")
 
+# Middle_Left class inheriting from Top
 class Middle_Left(Top):
-
+        # Method of the Middle_Left class
         def m_middle(self):
-
                 print("middle_left")
 
+# Middle_Right class inheriting from Top
 class Middle_Right(Top):
-
+        # Method of Middle_Right class
         def m_middle(self):
-
              print("middle_right")
 
+# Bottom class inehriting from both Middle_Left and Middle_Right
 class Bottom(Middle_Left, Middle_Right):
-
+        # Method of the Bottom class
         def m_bottom(self):
+               print("bottom")
 
-            print("bottom")
-
+# Create an instance of Bottom
 object = Bottom()
-
+# Call the m_bottom method from the Bottom class
 object.m_bottom()
-
+# Call the m_middle method
 object.m_middle()
-
+# Call the m_top method from the Top class
 object.m_top()
+
+"""
+The output of the program is:
+bottom
+middle_left
+top
+
+object.m_bottom() calls m_bottom from the Bottom class.
+object.m_middle() calls m_middle from the Middle_Left class due to the MRO, which places Middle_Left before Middle_Right.
+object.m_top() calls m_top from the Top class since neither Bottom nor Middle_Left nor Middle_Right override it.
+"""
+
--- collin/practice.py
+++ collin/practice.py
@@ -1,12 +1,7 @@
-class Level1:
-    var = 100
-    def fun(self):
-        return 101
-class Level2(Level1):
-    var = 200
-    def fun(self):
-        return 201
-class Level3(Level2):
-    pass
-obj = Level3()
-print(obj.var, obj.fun())
+try:
+  print(1/0)
+
+except ZeroDivisionError:
+   print("zero")
+except ArithmeticError:
+          print("arith")
